13066d563456b9d2da9969f101e08685ac93c562
Админ <admin@MacBook-Pro-Admin.local>
2020-10-23 17:25:09 +0400

    new start modal

--- src/components/StartModal/StartModal.js
+++ src/components/StartModal/StartModal.js
@@ -10,6 +10,32 @@ import {
 } from 'react-native';
 
 export default StartModal = ({startModal, hideModal}) => {
+  const imageArray = [
+    require('../../images/controls/instructions.png'),
+    require('../../images/controls/activeInstruction.png'),
+    require('../../images/controls/clearInstruction.png'),
+  ];
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const leftImage = () => {
+    let newIndex = imageIndex;
+    if (newIndex == 0) {
+      newIndex = 3;
+    }
+    newIndex--;
+    setImageIndex(newIndex);
+  };
+
+  const rightImage = () => {
+    let newIndex = imageIndex;
+    if (newIndex == 2) {
+      newIndex = -1;
+    }
+    newIndex++;
+    setImageIndex(newIndex);
+  };
+
   return (
     <Modal visible={startModal}>
       <View style={styles.header}>
@@ -18,10 +44,46 @@ export default StartModal = ({startModal, hideModal}) => {
           <Text style={styles.police}>Drive</Text>
           <Text style={styles.police}>Chat</Text>
         </View>
-        <Image
-          source={require('../../images/controls/instructions.png')}
-          style={{width: 270, height: 230, marginBottom: 90}}
-        />
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <TouchableOpacity style={styles.arrows} onPress={leftImage}>
+            <Image
+              source={require('../../images/controls/left.png')}
+              style={{width: 60, height: 30}}
+            />
+          </TouchableOpacity>
+          <Image
+            source={imageArray[imageIndex]}
+            style={{width: 270, height: 230, marginBottom: 90}}
+          />
+          <TouchableOpacity style={styles.arrows} onPress={rightImage}>
+            <Image
+              source={require('../../images/controls/right.png')}
+              style={{width: 60, height: 30}}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginBottom: 30,
+          }}>
+          <TouchableOpacity
+            onPress={() => setImageIndex(0)}
+            style={
+              imageIndex == 0 ? styles.activeRound : styles.round
+            }></TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setImageIndex(1)}
+            style={
+              imageIndex == 1 ? styles.activeRound : styles.round
+            }></TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setImageIndex(2)}
+            style={
+              imageIndex == 2 ? styles.activeRound : styles.round
+            }></TouchableOpacity>
+        </View>
         <TouchableOpacity
           style={styles.goToMyPositionIcon}
           onPress={() => hideModal()}>
@@ -38,13 +100,11 @@ export default StartModal = ({startModal, hideModal}) => {
 const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
-    justifyContent: 'center',
     marginTop: 100,
     marginBottom: 100,
   },
   welcomeText: {
     fontSize: 24,
-    //fontWeight: 'bold',
     color: '#777777',
   },
   police: {
@@ -55,6 +115,24 @@ const styles = StyleSheet.create({
   policegrammWrapper: {
     display: 'flex',
     flexDirection: 'row',
-    marginBottom: 70,
+    marginBottom: 30,
+  },
+  arrows: {
+    marginTop: 75,
+  },
+  round: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#dddada',
+    marginLeft: 5,
+  },
+  activeRound: {
+    width: 25,
+    height: 25,
+    borderRadius: 12.5,
+    backgroundColor: '#aeaeae',
+    marginTop: -2.5,
+    marginLeft: 5,
   },
 });
