daed29cc62c7aae6d0db40f7fc9a546009be37bb
Админ <admin@MacBook-Pro-Admin.local>
2020-10-24 16:03:09 +0400

    new icons and launchscreen

--- ios/DriveChatTests/DriveChatTests.m
+++ ios/DriveChatTests/DriveChatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MapChatTests : XCTestCase
+@interface DriveChatTests : XCTestCase
 
 @end
 
-@implementation MapChatTests
+@implementation DriveChatTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
